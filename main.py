6212f23a6bc2dc0506d8a484b21677a7fb36a166
vanti113 <vanti113@gmail.com>
2021-03-04 16:48:54 +0900

    lesson 2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("fuck that!")
+import pandas as pd
+
+daily_df = pd.read_csv("data/daily_report.csv")
+
+totals_df = daily_df[["Confirmed", "Deaths", "Recovered"]
+                     ].sum().reset_index(name="Count")
+totals_df = totals_df.rename(columns={"index": "conditions"})
+
+countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
+countries_df = countries_df.groupby("Country_Region").sum().reset_index()
+
+print(countries_df)
