c4a9fcc881b5c79a947c3433e93a924dc3fff7d5
Bae Kigun <vanti113@gmail.com>
2021-03-08 11:02:08 +0900

    lesson3

--- main.py
+++ main.py
@@ -8,5 +8,3 @@ totals_df = totals_df.rename(columns={"index": "conditions"})
 
 countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
 countries_df = countries_df.groupby("Country_Region").sum().reset_index()
-
-print(countries_df)
